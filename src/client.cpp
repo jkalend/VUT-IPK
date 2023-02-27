6b00839926d71f34a78e0e77bff5d43e524e0289
kalen <jkalend@gmail.com>
2023-02-27 04:03:00 +0100

    nearly done

--- src/client.cpp
+++ src/client.cpp
@@ -1,25 +1,24 @@
-/*
- * IPK.2015L
- *
- * Demonstration of trivial TCP communication.
- *
- * Ondrej Rysavy (rysavy@fit.vutbr.cz)
- *
- */
 #include <iostream>
+#include <fstream>
 #include <string>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
-#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <csignal>
+#include <bits/stdc++.h>
 
 #define BUFSIZE 1024
 #define UDP_BUFSIZE 512
+
+namespace csocket {
+	int client_socket;
+	struct addrinfo *serverptr;
+}
 
 int check_args(int argc, char **argv, const char **hostname, uint16_t *port, std::string *protocol) {
 	int c;
@@ -40,10 +39,8 @@ int check_args(int argc, char **argv, const char **hostname, uint16_t *port, std
 		}
 	}
 
-	if (*endptr != '\0')
-			throw std::runtime_error("ERROR: invalid port");
-
-	//std::cout << "here" << std::endl;
+	if (endptr != nullptr && *endptr != '\0')
+		throw std::runtime_error("ERROR: invalid port");
 
 	if (*protocol != "tcp" && *protocol != "udp")
 		throw std::runtime_error("ERROR: invalid protocol");
@@ -52,44 +49,30 @@ int check_args(int argc, char **argv, const char **hostname, uint16_t *port, std
 }
 
 struct sockaddr_in * get_adress(const char *hostname) {
-	/* 2. ziskani adresy serveru pomoci DNS */
-	struct addrinfo hints = {0};
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_DGRAM;
-	hints.ai_flags = AI_PASSIVE;
-	hints.ai_protocol = 0;
+	struct addrinfo hints = {AI_PASSIVE, AF_INET, SOCK_DGRAM, 0, 0, nullptr, nullptr, nullptr};
 
-	struct addrinfo *serverptr = {0};
+	csocket::serverptr = nullptr;
 
-	if (getaddrinfo(hostname, nullptr, &hints, &serverptr) != 0) {
-		perror("ERROR: no such host exists");
-        exit(EXIT_FAILURE);
-    }
-	return (struct sockaddr_in*)(serverptr->ai_addr);
+	if (getaddrinfo(hostname, nullptr, &hints, &csocket::serverptr) != 0)
+		throw std::runtime_error("ERR: getaddrinfo failed");
+
+	return (struct sockaddr_in*)(csocket::serverptr->ai_addr);
 }
 
 int tcp_socket(struct sockaddr_in server_address) {
 	int client_socket;
-	if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) <= 0) {
-		//perror("ERROR: socket creation failed");
-		throw std::runtime_error("ERROR: socket connection failed");
-		//exit(EXIT_FAILURE);
-	}
+	if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) <= 0)
+		throw std::runtime_error("ERROR: socket creation failed");
 
-	if (connect(client_socket, (const struct sockaddr *) &server_address, sizeof(server_address)) != 0) {
-		//perror("ERROR: socket connection failed");
+	if (connect(client_socket, (const struct sockaddr *) &server_address, sizeof(server_address)) != 0)
 		throw std::runtime_error("ERROR: socket connection failed");
-		//exit(EXIT_FAILURE);
-	}
 	return client_socket;
 }
 
 int udp_socket() {
 	int client_socket;
-	if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) <= 0) {
-		perror("ERROR: socket creation failed");
-		exit(EXIT_FAILURE);
-	}
+	if ((client_socket = socket(AF_INET, SOCK_DGRAM, 0)) <= 0)
+		throw std::runtime_error("ERROR: socket creation failed");
 	return client_socket;
 }
 
@@ -101,26 +84,26 @@ void tcp_communicate(int client_socket) {
 		std::getline(std::cin, buff);
 		buff += "\n";
 		if (buff.length() > BUFSIZE) {
-			std::cout << "Message is too long" << std::endl;
+			std::cerr << "Message is too long" << std::endl;
 			continue;
 		}
-		strcpy(buf, buff.data());
 
-		/* odeslani zpravy na server */
-		if (ssize_t sent = send(client_socket, buf, strlen(buf), 0); sent < 0)
-			perror("ERROR in sendto");
+		if (ssize_t sent = send(client_socket, buff.data(), buff.length(), 0); sent < 0)
+			throw std::runtime_error("ERR: message sending failed");
 
 
-		/* prijeti odpovedi a jeji vypsani */
-		memset(buf, 0, BUFSIZE);
 		if (ssize_t res = recv(client_socket, buf, BUFSIZE, 0); res < 0)
-			perror("ERROR in recvfrom");
+			throw std::runtime_error("ERR: message receiving failed");
 
-		printf("Echo from server: %s", buf);
+		std::cout << buf;
 
-		if (strcmp(buf, "BYE\n") == 0) {
+		if (strcmp(buf, "BYE\n") == 0 && buff != "BYE\n")
+			throw std::runtime_error("ERR: server closed connection due to invalid request");
+
+		if (strcmp(buf, "BYE\n") == 0 && buff == "BYE\n")
 			break;
-		}
+
+		memset(buf, 0, BUFSIZE);
 	}
 }
 
@@ -132,81 +115,84 @@ void udp_communicate(int client_socket, struct sockaddr_in server_address, sockl
 		std::string buff;
 		socklen_t len;
 		std::getline(std::cin, buff);
-		buff += "\n";
+		if (std::cin.eof())
+			break;
+
+		std::string start(1, (char)(buff.length() + 1));
+		buff = '\0' + start + buff + '\n';
 		if (buff.length() > UDP_BUFSIZE) {
 			std::cout << "Message is too long" << std::endl;
 			continue;
 		}
-		strcpy(buf, buff.data());
-//		for (char i : buf) {
-//			std::cout << (int)i << " ";
-//		}
-//		exit(0);
-//		bzero(buf, UDP_BUFSIZE);
-//		fgets(buf, UDP_BUFSIZE, stdin);
 
-		/* odeslani zpravy na server */
-		if (ssize_t sent = sendto(client_socket, (const char *)buf, strlen(buf), MSG_CONFIRM, (const struct sockaddr *) &server_address, server_address_len); sent < 0)
+		if (ssize_t sent = sendto(client_socket, buff.data(), buff.length(), MSG_CONFIRM, (struct sockaddr *) &server_address, server_address_len); sent < 0)
 			perror("ERROR in sendto");
 
 		/* prijeti odpovedi a jeji vypsani */
 		memset(buf, 0, UDP_BUFSIZE);
-		ssize_t res = recvfrom(client_socket, (char *)buf, UDP_BUFSIZE, MSG_WAITALL, (struct sockaddr *) &server_address, &len);
-		if (res < 0)
+		if (ssize_t res = recvfrom(client_socket, (char *)buf, UDP_BUFSIZE, MSG_WAITALL, (struct sockaddr *) &server_address, &len); res < 0)
 			perror("ERROR in recvfrom");
 
-		buf[res] = '\0';
-		std::cout << "Echo from server: " << buf << std::endl;
-		//printf("Echo from server: %s", buf);
-
-		if (strcmp(buf, "BYE\n") == 0) {
-			break;
+		if (buf[1] == 0) {
+			std::cout << "OK:" << buf + 3 << std::endl;
+		} else {
+			std::cout << "ERR: parsing failed" << std::endl;
 		}
 	}
 }
 
-int main (int argc, char **argv) {
-	uint16_t port_number;
-	std::string protocol;
-	int client_socket;
-    const char *server_hostname;
-    struct sockaddr_in server_address;
+void sigint_handler(int) {
+	std::cout << "Exiting" << std::endl;
+	std::cout << "Bye..." << std::endl;
+	freeaddrinfo(csocket::serverptr);
+	close(csocket::client_socket);
+	exit(EXIT_SUCCESS);
+}
 
-    /* 1. test vstupnich parametru: */
+int main (int argc, char **argv) {
+	uint16_t port_number = 0;
+	std::string protocol;
+    const char *server_hostname = nullptr;
+    struct sockaddr_in server_address = {0, 0, 0, 0};
+
+	std::signal(SIGINT, sigint_handler);
+
 	try {
         check_args(argc, argv, &server_hostname, &port_number, &protocol);
+		server_address = *get_adress(server_hostname);
+        server_address.sin_port = htons(port_number);
     } catch (std::runtime_error &e) {
-		std::cout << e.what() << std::endl;
-		exit(EXIT_FAILURE);
+		std::cerr << e.what() << std::endl;
+		return 1;
 	}
 
-    /* 3. nalezeni IP adresy serveru a inicializace struktury server_address */
-	server_address = *get_adress(server_hostname);
-    server_address.sin_port = htons(port_number);
-
-    /* tiskne informace o vzdalenem soketu */ 
-    printf("INFO: Server socket: %s : %d \n", inet_ntoa(server_address.sin_addr), ntohs(server_address.sin_port));
+    //printf("INFO: Server socket: %s : %d \n", inet_ntoa(server_address.sin_addr), ntohs(server_address.sin_port));
     
-    /* Vytvoreni soketu */
 	try {
 		if (protocol == "tcp") {
-			client_socket = tcp_socket(server_address);
+			csocket::client_socket = tcp_socket(server_address);
 		} else {
-			client_socket = udp_socket();
+			csocket::client_socket = udp_socket();
 		}
 	} catch (std::runtime_error &e) {
 		std::cout << e.what() << std::endl;
-		exit(EXIT_FAILURE);
+		freeaddrinfo(csocket::serverptr);
+		return 1;
 	}
 
-	/* komunikace s serverem */
-	if (protocol == "tcp") {
-		tcp_communicate(client_socket);
-	}
-	if (protocol == "udp") {
-		udp_communicate(client_socket, server_address, sizeof(server_address));
+	try {
+		if (protocol == "tcp")
+			tcp_communicate(csocket::client_socket);
+		if (protocol == "udp")
+			udp_communicate(csocket::client_socket, server_address, sizeof(server_address));
+	} catch (std::runtime_error &e) {
+		std::cout << e.what() << std::endl;
+		close(csocket::client_socket);
+		freeaddrinfo(csocket::serverptr);
+		return 1;
 	}
 
-    close(client_socket);
+	freeaddrinfo(csocket::serverptr);
+    close(csocket::client_socket);
     return 0;
 }
